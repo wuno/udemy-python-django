9158bf6ca5cc69aaf74e228a65b03d578ff770af
wuno <ndiaz@wuno.com>
2016-03-19 15:23:57 -0500

    Starting front end dev, Removed store.html which is now base.html

--- store/views.py
+++ store/views.py
@@ -8,8 +8,9 @@ def store(request):
 	count = Book.objects.all().count()
 	context = {
 		'count': count,
+		'page': 'Welcome to my super cool app!',
 	}
 	request.session['location'] = "unknown"
 	if request.user.is_authenticated():
 		request.session['location'] = "Earth"
-	return render(request, 'store.html', context)
+	return render(request, 'base.html', context)
