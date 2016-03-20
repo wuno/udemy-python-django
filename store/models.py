22da81e0b37eca483039b3cdd64caac11bc37401
wuno <ndiaz@wuno.com>
2016-03-20 15:36:35 -0500

    Added payment methods, paypal payment api, stripe payment api, added jquery, added views

--- store/models.py
+++ store/models.py
@@ -30,31 +30,32 @@ class Cart(models.Model):
 	payment_type = models.CharField(max_length=100, null=True)
 	payment_id = models.CharField(max_length=100, null=True)
 
-	def add_to_cart(self, book_id):
-		book = Book.objects.get(pk=book_id)
-		try:
-			preexisting_order = BookOrder.objects.get(book=book, cart=self)
-			preexisting_order.quantity += 1
-			preexisting_order.save()
-		except BookOrder.DoesNotExist:
-			new_order = BookOrder.objects.create(
-				book=book,
-				cart=self,
-				quantity=1
-				)
-			new_order.save()
+def add_to_cart(self, book_id):
+	book = Book.objects.get(pk=book_id)
+	try:
+		preexisting_order = BookOrder.objects.get(book=book, cart=self)
+		preexisting_order.quantity += 1
+		preexisting_order.save()
+	except BookOrder.DoesNotExist:
+		new_order = BookOrder.objects.create(
+			book=book,
+			cart=self,
+			quantity=1
+			)
+		new_order.save()
 
-	def remove_to_cart(self, book_id):
-		book = Book.objects.get(pk=book_id)
-		try:
-			preexisting_order = BookOrder.objects.get(book=book, cart=self)
-			if preexisting_order.quantity > 1:
-				preexisting_order.quantity -= 1
-				preexisting_order.save()
-			else:
-				preexisting_order.delete()
-		except BookOrder.DoesNotExist:
-			pass
+def remove_to_cart(self, book_id):
+	book = Book.objects.get(pk=book_id)
+	try:
+		preexisting_order = BookOrder.objects.get(book=book, cart=self)
+		if preexisting_order.quantity > 1:
+			preexisting_order.quantity -= 1
+			preexisting_order.save()
+		else:
+			preexisting_order.delete()
+	except BookOrder.DoesNotExist:
+		pass
+
 
 class BookOrder(models.Model):
 	book = models.ForeignKey(Book)
