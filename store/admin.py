6b889f4f7de848968fdb4b707af1d192d69d85e4
wuno <ndiaz@wuno.com>
2016-03-20 03:05:08 -0500

    Added ecommerce module, created book details view for displaying books, creeated view displaying information for each  book

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from.models import Book
+from.models import Book, Author
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-	list_display = ('title', 'author')
+	list_display = ('title', 'price', 'stock')
+
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name')
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
